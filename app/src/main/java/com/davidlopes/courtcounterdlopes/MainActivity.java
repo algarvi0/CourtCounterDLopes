f29f274f5e37961f69509f3e012604a500990f08
algarvi0 <algarvi0@gmail.com>
2017-12-20 12:53:23 +0000

    - ScrolView fixed. - Some java fixed. - Luncher Icon.

--- app/src/main/java/com/davidlopes/courtcounterdlopes/MainActivity.java
+++ app/src/main/java/com/davidlopes/courtcounterdlopes/MainActivity.java
@@ -5,9 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
-
     // Set static for Restore and Save values
     static final String SCORE_TEAM_A_VALUE = "scoreA";
     static final String FAULT_TEAM_A_VALUE = "faultA";
@@ -34,7 +32,8 @@ public class MainActivity extends AppCompatActivity {
     int faultTeamB = 0;
     int reboundsTeamB = 0;
     int stealsTeamB = 0;
-
+    //
+    TextView scoreView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,12 +53,9 @@ public class MainActivity extends AppCompatActivity {
         savedInstanceState.putInt(FAULT_TEAM_B_VALUE, faultTeamB);
         savedInstanceState.putInt(REBOUNDS_TEAM_B_VALUE, reboundsTeamB);
         savedInstanceState.putInt(STEALS_TEAM_B_VALUE, stealsTeamB);
-
         // Always call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
     }
-
-
     // Restore scores and details after rotation
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
@@ -74,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         displayReboundsTeamA(reboundsTeamA);
         stealsTeamA = savedInstanceState.getInt(STEALS_TEAM_A_VALUE);
         displayStealsTeamA(stealsTeamA);
-
         // Restore Team B details from saved instance
         scoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B_VALUE);
         displayScoreForTeamB(scoreTeamB);
@@ -84,144 +79,113 @@ public class MainActivity extends AppCompatActivity {
         displayReboundsTeamB(reboundsTeamB);
         stealsTeamB = savedInstanceState.getInt(STEALS_TEAM_B_VALUE);
         displayStealsTeamB(stealsTeamB);
-
     }
-
     // TEAM A
-
     // Sets 3 points for Team A.
     public void set3Points_teamA(View view) {
         scoreTeamA = scoreTeamA + TREE_POINTS;
         displayScoreForTeamA(scoreTeamA);
     }
-
     // Sets 2 points for Team A
     public void set2Points_teamA(View view) {
         scoreTeamA = scoreTeamA + TWO_POINTS;
         displayScoreForTeamA(scoreTeamA);
     }
-
     // Sets FreeThrow - 1 points for Team A
     public void setFreeThrow_teamA(View view) {
         scoreTeamA = scoreTeamA + ONE_POINT;
         displayScoreForTeamA(scoreTeamA);
     }
-
     // Displays the given score for Team A.
     public void displayScoreForTeamA(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
     // Faults
-
     // Sets Fault - 1
     public void setFaults_teamA(View view) {
         faultTeamA = faultTeamA + ONE_POINT;
         displayFaultTeamA(faultTeamA);
     }
-
     // Displays fault for Team A.
     public void displayFaultTeamA(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.faultsTeamA_value);
         scoreView.setText(String.valueOf(score));
     }
-
     // Rebounds
     // Sets Rebounds +  1
     public void setRebounds_teamA(View view) {
         reboundsTeamA = reboundsTeamA + ONE_POINT;
         displayReboundsTeamA(reboundsTeamA);
     }
-
     // Displays fault for Team A.
     public void displayReboundsTeamA(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.reboundsTeamA_value);
         scoreView.setText(String.valueOf(score));
     }
-
     // Sets Steals +  1
     public void setSteals_teamA(View view) {
         stealsTeamA = stealsTeamA + ONE_POINT;
         displayStealsTeamA(stealsTeamA);
     }
-
     // Displays Steals for Team A.
     public void displayStealsTeamA(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.stealsTeamA_value);
         scoreView.setText(String.valueOf(score));
     }
-
     // TEAM B
     // Sets 3 points for Team B.
     public void set3Points_teamB(View view) {
         scoreTeamB = scoreTeamB + TREE_POINTS;
         displayScoreForTeamB(scoreTeamB);
     }
-
     // Sets 2 points for Team B
     public void set2Points_teamB(View view) {
         scoreTeamB = scoreTeamB + TWO_POINTS;
         displayScoreForTeamB(scoreTeamB);
     }
-
     // Sets FreeThrow - 1 points for Team B
     public void setFreeThrow_teamB(View view) {
         scoreTeamB = scoreTeamB + ONE_POINT;
         displayScoreForTeamB(scoreTeamB);
     }
-
     // Displays the given score for Team B.
     public void displayScoreForTeamB(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
     }
-
     // Faults
     // Sets Fault - 1
     public void setFaults_teamB(View view) {
         faultTeamB = faultTeamB + ONE_POINT;
         displayFaultTeamB(faultTeamB);
     }
-
     // Displays fault for Team A.
     public void displayFaultTeamB(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.faultsTeamB_value);
         scoreView.setText(String.valueOf(score));
     }
-
     // Rebounds
     // Sets Rebounds +  1
     public void setRebounds_teamB(View view) {
         reboundsTeamB = reboundsTeamB + ONE_POINT;
         displayReboundsTeamB(reboundsTeamB);
     }
-
     // Displays fault for Team A.
     public void displayReboundsTeamB(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.reboundsTeamB_value);
         scoreView.setText(String.valueOf(score));
     }
-
     // Sets Steals +  1
     public void setSteals_teamB(View view) {
         stealsTeamB = stealsTeamB + ONE_POINT;
         displayStealsTeamB(stealsTeamB);
     }
-
     // Displays Steals for Team A.
     public void displayStealsTeamB(int score) {
-        TextView scoreView;
         scoreView = (TextView) findViewById(R.id.stealsTeamB_value);
         scoreView.setText(String.valueOf(score));
     }
-
     // Reset Teams Counter
     public void resetScores(View view) {
         // Team A reset
